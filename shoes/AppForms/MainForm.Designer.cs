48e1be5666431af6540c047be74c469ea1c4b446
Игорь Шапкин <shapkin.igorek@gmail.com>
2025-10-30 10:34:32 +0300

    Created "Proucts" page base design

--- shoes/AppForms/MainForm.Designer.cs
+++ shoes/AppForms/MainForm.Designer.cs
@@ -29,12 +29,105 @@ namespace shoes.AppForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.formSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.headerLogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.contentFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.headerTitleLabel = new System.Windows.Forms.Label();
+            this.headerSubtitleLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.formSplitContainer)).BeginInit();
+            this.formSplitContainer.Panel1.SuspendLayout();
+            this.formSplitContainer.Panel2.SuspendLayout();
+            this.formSplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.headerLogoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // formSplitContainer
+            // 
+            this.formSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formSplitContainer.IsSplitterFixed = true;
+            this.formSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.formSplitContainer.Name = "formSplitContainer";
+            this.formSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // formSplitContainer.Panel1
+            // 
+            this.formSplitContainer.Panel1.BackColor = System.Drawing.Color.Chartreuse;
+            this.formSplitContainer.Panel1.Controls.Add(this.headerSubtitleLabel);
+            this.formSplitContainer.Panel1.Controls.Add(this.headerTitleLabel);
+            this.formSplitContainer.Panel1.Controls.Add(this.headerLogoPictureBox);
+            // 
+            // formSplitContainer.Panel2
+            // 
+            this.formSplitContainer.Panel2.Controls.Add(this.contentFlowLayoutPanel);
+            this.formSplitContainer.Size = new System.Drawing.Size(800, 450);
+            this.formSplitContainer.SplitterDistance = 100;
+            this.formSplitContainer.TabIndex = 0;
+            // 
+            // headerLogoPictureBox
+            // 
+            this.headerLogoPictureBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.headerLogoPictureBox.Image = global::shoes.Properties.Resources.Icon2;
+            this.headerLogoPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.headerLogoPictureBox.Name = "headerLogoPictureBox";
+            this.headerLogoPictureBox.Size = new System.Drawing.Size(100, 100);
+            this.headerLogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.headerLogoPictureBox.TabIndex = 0;
+            this.headerLogoPictureBox.TabStop = false;
+            // 
+            // contentFlowLayoutPanel
+            // 
+            this.contentFlowLayoutPanel.BackColor = System.Drawing.Color.White;
+            this.contentFlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.contentFlowLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.contentFlowLayoutPanel.Name = "contentFlowLayoutPanel";
+            this.contentFlowLayoutPanel.Size = new System.Drawing.Size(800, 346);
+            this.contentFlowLayoutPanel.TabIndex = 0;
+            // 
+            // headerTitleLabel
+            // 
+            this.headerTitleLabel.AutoSize = true;
+            this.headerTitleLabel.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.headerTitleLabel.Location = new System.Drawing.Point(106, 25);
+            this.headerTitleLabel.Name = "headerTitleLabel";
+            this.headerTitleLabel.Size = new System.Drawing.Size(141, 23);
+            this.headerTitleLabel.TabIndex = 1;
+            this.headerTitleLabel.Text = "ООО \"Обувь\"";
+            // 
+            // headerSubtitleLabel
+            // 
+            this.headerSubtitleLabel.AutoSize = true;
+            this.headerSubtitleLabel.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.headerSubtitleLabel.Location = new System.Drawing.Point(106, 50);
+            this.headerSubtitleLabel.Name = "headerSubtitleLabel";
+            this.headerSubtitleLabel.Size = new System.Drawing.Size(52, 16);
+            this.headerSubtitleLabel.TabIndex = 2;
+            this.headerSubtitleLabel.Text = "Товары";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            this.Controls.Add(this.formSplitContainer);
+            this.Name = "MainForm";
+            this.Text = "ООО \"Обувь\"";
+            this.formSplitContainer.Panel1.ResumeLayout(false);
+            this.formSplitContainer.Panel1.PerformLayout();
+            this.formSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.formSplitContainer)).EndInit();
+            this.formSplitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.headerLogoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer formSplitContainer;
+        private System.Windows.Forms.PictureBox headerLogoPictureBox;
+        private System.Windows.Forms.FlowLayoutPanel contentFlowLayoutPanel;
+        private System.Windows.Forms.Label headerSubtitleLabel;
+        private System.Windows.Forms.Label headerTitleLabel;
     }
 }
